4d735b35458ffa62eb5f902d4107d92b610eb98c
RyanMElliot <rmelliot1@sheffield.ac.uk>
2026-05-29 12:11:18 +0100

    rsync utils update for new laptop

--- rpush.sh
+++ rpush.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --progress ./ Stanage:/mnt/parscratch/users/smp24rme/ezcascades --exclude='.git/*' --exclude='*.ipynb_checkpoints'
+rsync -av --progress ./ stanage:/mnt/parscratch/users/smp24rme/ezcascades --exclude='.git/*' --exclude='*.ipynb_checkpoints'
